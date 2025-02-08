84dbd9bcb6917e85857e754dd03e79303b29e4f6
Rachit Bharadwaj <rachitbharadwaj52@gmail.com>
2025-02-09 00:01:45 +0530

    fix: removed the App.tsx file

--- template/src/root.tsx
+++ template/src/root.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
-import './styles/global.css';
+import { RouterProvider } from "react-router-dom";
+import { router } from "../.revine/routing/fileBased";
+import "./styles/global.css";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />;
   </React.StrictMode>
 );
